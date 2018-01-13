1e434b90073479b6bfeeaae247e6706b6ca5bb69
brainylark <erdenebileg.byambadorj@gmail.com>
2018-01-13 21:42:51 +0800

    Meta modified

--- api/domain/domain.controller.js
+++ api/domain/domain.controller.js
@@ -4,11 +4,7 @@ const Domain 	= require('./domain.model.js')
 const meta		= require('../../meta')
 
 //global ids for the twenty-five unique beginner concepts
-const uids = [
-	161, 119, 124, 129, 15355, 127, 145, 138,
-	131, 123, 40, 132, 83505, 121, 61558, 118, 120,
-	143, 139, 28162, 142, 133, 130, 122, 134
-]
+const uids = meta.ub_ids
 
 const selection = {
 	"synset": false,
--- meta/index.js
+++ meta/index.js
@@ -22,6 +22,11 @@ module.exports = {
 		ongoing: false,
 		terminated: true
 	},
+	ub_ids: [
+		161, 119, 124, 129, 15355, 127, 145, 138,
+		131, 123, 40, 132, 83505, 121, 61558, 118, 120,
+		143, 139, 28162, 142, 133, 130, 122, 134
+	],
 	interval: 10000,// * dayInMilli,
 	languageCodes: [
 		'en', 'in', 'it', 'es', 'zh'
