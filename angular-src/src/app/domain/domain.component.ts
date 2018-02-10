d2cf3701a4ee55cbbda7943472897b3e26e5addc
brainylark <erdenebileg.byambadorj@gmail.com>
2018-02-10 18:09:08 +0800

    Domain design has been established

--- angular-src/src/app/domain/domain.component.ts
+++ angular-src/src/app/domain/domain.component.ts
@@ -10,6 +10,14 @@ import { Domain } from '../model/Domain';
 })
 export class DomainComponent implements OnInit {
 	
+	current_v: number = 1;
+	navLinks = [
+		{ vid: 1, lang: "Англи", isActive: true },
+		{ vid: 102, lang: "Итали", isActive: false },
+		{ vid: 101, lang: "Испани", isActive: false },
+		{ vid: 106, lang: "Хятад", isActive: false },
+		{ vid: 401, lang: "Индонез", isActive: false }
+	];
 	domains: Domain[];
 	placeholder = [];
 
@@ -19,21 +27,28 @@ export class DomainComponent implements OnInit {
 		let jwt_token = localStorage.getItem('jwt_token');
 		if (jwt_token) {
 			this.domainService.getDomains(jwt_token).subscribe(res => {
-				if (res) this.domains = res;
-				else this.router.navigateByUrl('/');
+				if (res) { 
+					this.domains = res;
+					this.updateLang(this.current_v);
+				}
+				else { 
+					this.router.navigateByUrl('/');
+				}
 			})
 		} else {
 			this.router.navigateByUrl('/');
 		}
 	}
 
-	updateSynsetLanguage(requestedCode) {
+	updateLang(requestedCode) {
 		this.placeholder = [];
 		const defaultCode = 1;
-		let counter = 0;
+		this.navLinks.filter(link => link.vid == this.current_v)[0].isActive = false;
+		this.navLinks.filter(link => link.vid == requestedCode)[0].isActive = true;
+		this.current_v = requestedCode;
 		this.domains.forEach(domain => {
 			let synset = domain.synset.filter((s) => s.vocabularyId == requestedCode);
-			if (!synset.length) {
+			if (synset.length == 0) {
 				synset = domain.synset.filter((s) => s.vocabularyId == defaultCode);
 			}
 			this.placeholder.push(synset[0]);
--- angular-src/src/app/material.module.ts
+++ angular-src/src/app/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatGridListModule, MatButtonModule, MatToolbarModule, MatCardModule, MatInputModule, MatIconModule, MatProgressSpinnerModule, MatStepperModule, MatFormFieldModule, MatSnackBarModule, MatMenuModule } from '@angular/material';
+import { MatTabsModule, MatGridListModule, MatButtonModule, MatToolbarModule, MatCardModule, MatInputModule, MatIconModule, MatProgressSpinnerModule, MatStepperModule, MatFormFieldModule, MatSnackBarModule, MatMenuModule } from '@angular/material';
 
 @NgModule({
-	imports: [MatGridListModule, MatButtonModule, MatToolbarModule, MatProgressSpinnerModule, MatIconModule, MatInputModule, MatCardModule, MatStepperModule, MatFormFieldModule, MatSnackBarModule, MatMenuModule],
-	exports: [MatGridListModule, MatButtonModule, MatToolbarModule, MatProgressSpinnerModule, MatIconModule, MatInputModule, MatCardModule, MatStepperModule, MatFormFieldModule, MatSnackBarModule, MatMenuModule],
+	imports: [MatTabsModule, MatGridListModule, MatButtonModule, MatToolbarModule, MatProgressSpinnerModule, MatIconModule, MatInputModule, MatCardModule, MatStepperModule, MatFormFieldModule, MatSnackBarModule, MatMenuModule],
+	exports: [MatTabsModule, MatGridListModule, MatButtonModule, MatToolbarModule, MatProgressSpinnerModule, MatIconModule, MatInputModule, MatCardModule, MatStepperModule, MatFormFieldModule, MatSnackBarModule, MatMenuModule],
 })
 
 export class MaterialModule { }
